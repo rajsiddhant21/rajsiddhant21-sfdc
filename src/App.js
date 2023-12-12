230af1ef95e34fc36635fb00ce74367346700c8e
Siddhant Raj <1602125@kiit.ac.in>
2023-12-12 15:18:07 +0530

    api working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
-import { BrowserRouter, HashRouter, Navigate, Route, RouterProvider, Routes, createHashRouter } from 'react-router-dom';
+
+
+const tokenurl = `https://login.salesforce.com/services/oauth2/token?username=sid1707raj@empathetic-panda-tmx0kp.com&password=Needasalesforcejob!@%2312356EFK3AsBums8qIWv6IPhWHcR&client_id=3MVG9n_HvETGhr3CqcsKNtCUjlqTSFA6GOMELXkxhn6eawqks641sqCpWa70nX2gGZKAFWqLu2AlUquo7PTa3&client_secret=507B93FF3411DF4351C9C722159FFF1E6D13F370675A4E766206A4C3D160DDAF&grant_type=password`;
 
 function App() {
 
-  const routersO = createHashRouter([
-    {
-      "path": "/",
-      "element": <h1>Home</h1>
+  useEffect(function () {
+    async function token() {
+      console.log('calling fetch')
+      fetch(tokenurl, {
+        method: 'POST',
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+        .then((res) => {
+          console.log(res)
+          return res.json();
+        })
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+
     }
-  ])
+
+    token();
+  })
+
+  useEffect(function () {
+    async function getCv() {
+
+      fetch(`
+      https://empathetic-panda-tmx0kp-dev-ed.trailblaze.my.salesforce.com/services/apexrest/resume/user?id=siddhantraj
+      `, {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer 00D2w00000GWHJ2!AQYAQKvCBCkyZ6hP07HCLfdHmx_dCJ1_X5VozGkKBPXbKd25CortaCfcqEtms1GqmzBjHKhf7Uw9Wv.tUVEt0yo9cHJq_mmq'
+        }
+      }).then((res) => {
+        console.log(res)
+        return res.json();
+      })
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+
+    }
+
+    getCv();
+
+  })
 
   return (
-    <HashRouter>
-      <RouterProvider
-        router={routersO}></RouterProvider>
-    </HashRouter>
+    <div>
+      Data Incoming
+    </div>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,12 +4,7 @@ function Navigation() {
     return (
         <div>
             <ul>
-                <li>
-                    <NavLink to="home">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="about">About</NavLink>
-                </li>
+
             </ul>
         </div>
     )
