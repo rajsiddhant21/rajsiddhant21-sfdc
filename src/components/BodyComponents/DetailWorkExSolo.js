aa9cc70ca173ea153d0be0aa9d457e05d108190e
Raj <siddhant.raj@accenture.com>
2024-02-23 00:31:11 +0530

    accordian started , projects[] added

---
+++ src/components/BodyComponents/DetailWorkExSolo.js
@@ -0,0 +1,56 @@
+import styles from './DetailWorkExSolo.module.css';
+import { useEffect, useState } from 'react'
+import Pagination from './Pagination'
+import { Co2Sharp } from '@mui/icons-material';
+function DetailedWorkExSolo({state}){
+    return (
+        <div className={styles.workexdetail}>
+            <div className={styles.workexdesc}>
+                <div className={styles.salescloud + ' ' + styles.staticHeight}>
+                    <div className={styles.expBgAlpha}>
+                        <WorkExDesc project="project1" state={state} />
+                    </div>
+                </div>
+                <div className={styles.expcloud1 + ' ' + styles.staticHeight}>
+                    <div className={styles.expBgAlpha}>
+                        <WorkExDesc project="project2" state={state} />
+                    </div>
+                </div>
+                <div className={styles.expcloud2 + ' ' + styles.staticHeight}>
+                    <div className={styles.expBgAlpha}>
+                        <WorkExDesc project="project3" state={state} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function WorkExDesc({ state, project }) {
+    let MaxPage = 1;
+    let teamkey = '';
+    console.log(state);
+    const [activePage, setactivePage] = useState(1);
+    if (state && state[project]) {
+        MaxPage = Object.getOwnPropertyNames(state[project]).length
+    }
+    if (activePage) {
+        teamkey = 'team' + activePage;
+    }
+    return (<>
+        <div className={styles.workDesc}>
+            {state && <WorkExSingle desckey={teamkey} state={state[project]} />}
+        </div>
+        <div>
+            <Pagination maxPage={MaxPage} active={activePage} setActive={setactivePage} />
+        </div>
+    </>)
+}
+
+function WorkExSingle({ desckey, state }) {
+    return <>
+        {state[desckey]}
+    </>
+}
+
+export default DetailedWorkExSolo;
--- src/components/BodyComponents/DetailedWorkEx.js
+++ src/components/BodyComponents/DetailedWorkEx.js
@@ -1,57 +1,13 @@
-import { useEffect, useState } from 'react'
-import styles from './DetailedWorkEx.module.css'
-import Pagination from './Pagination'
+import DetailedWorkExSolo from "./DetailWorkExSolo"
 function DetailedWorkEx({ state }) {
     // let ht = 0.4 * (window.innerHeight)
     return (
-        <div className={styles.workexdetail}>
-            <div className={styles.workexdesc}>
-                <div className={styles.salescloud + ' ' + styles.staticHeight}>
-                    <div className={styles.expBgAlpha}>
-                        <WorkExDesc project="project1" state={state} />
-                    </div>
-                </div>
-                <div className={styles.expcloud1 + ' ' + styles.staticHeight}>
-                    <div className={styles.expBgAlpha}>
-                        <WorkExDesc project="project2" state={state} />
-                    </div>
-                </div>
-                <div className={styles.expcloud2 + ' ' + styles.staticHeight}>
-                    <div className={styles.expBgAlpha}>
-                        <WorkExDesc project="project3" state={state} />
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+        {state?.projects?.map((x)=>{
+            return <DetailedWorkExSolo state={x}/>
+        })}
+        </>
     )
-}
-
-
-
-function WorkExDesc({ state, project }) {
-    let MaxPage = 1;
-    let teamkey = '';
-    const [activePage, setactivePage] = useState(1);
-    if (state && state[project]) {
-        MaxPage = Object.getOwnPropertyNames(state[project]).length
-    }
-    if (activePage) {
-        teamkey = 'team' + activePage;
-    }
-    return (<>
-        <div className={styles.workDesc}>
-            {state && <WorkExSingle desckey={teamkey} state={state[project]} />}
-        </div>
-        <div>
-            <Pagination maxPage={MaxPage} active={activePage} setActive={setactivePage} />
-        </div>
-    </>)
-}
-
-function WorkExSingle({ desckey, state }) {
-    return <>
-        {state[desckey]}
-    </>
 }
 
 export default DetailedWorkEx
